56455d7d6e32f1d89cb2f7f77c466b36be7d5bff
KURORYOU <josuemesa28Gmail.com>
2023-02-16 20:47:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Tarea 3 - Agenda Multicapas:
+
+![Mi captura de pantalla](img/Cap1.png)
+![Mi captura de pantalla](img/Cap2.png)
---
+++ js/main.js
@@ -0,0 +1,22 @@
+function cargarContactos() {
+    fetch("http://www.raydelto.org/agenda.php").then(function(contactos){
+        return contactos.json();
+    }).then(function(contactos){
+        buildTable(contactos);
+    });
+}
+
+function buildTable(data){
+    var table = document.getElementById("my_table");
+
+    for(var i = 0; i<data.length; i++){
+        var row = `
+            <tr>
+                <td>${data[i].nombre}</td>
+                <td>${data[i].apellido}</td>
+                <td>${data[i].telefono}</td>
+            </tr>
+        `;
+        table.innerHTML += row
+    }
+}
